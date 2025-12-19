25d2fcfdbeafc839351c3be2e5cfc6cc48c683a8
IT0293 <IT0293@itc.in.th>
2025-12-19 14:01:54 +0700

    Remove unused backend server and old startup scripts - using unified server.js only

--- start-frontend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Frontend start script for PM2 on Windows
-cd /d "%~dp0frontend"
-npm run preview
--- start-frontend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Frontend start script for PM2
-cd "$(dirname "$0")/frontend"
-npm run preview
